9ff5aa4a11dfaba48ef95dd3cc89b16dbb7c5477
Frischifrisch <1000.gigabit.pro.sekunde@gmail.com>
2026-03-05 14:56:49 +0100

    Rechner vervollstaendigt: Hilfe, Eingabeschleife, alle 4 Grundrechenarten

--- ProjektrechnerTutorial.cpp
+++ ProjektrechnerTutorial.cpp
@@ -2,23 +2,96 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+void printHelp()
 {
-    cout << "Calculator Console Application" << endl << endl;
-    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
-        << endl;
-    return 0;
+    cout << "========================================" << endl;
+    cout << "      Calculator Console Application    " << endl;
+    cout << "========================================" << endl << endl;
+    cout << "Format:  a+b  |  a-b  |  a*b  |  a/b" << endl << endl;
+    cout << "Beispiele:" << endl;
+    cout << "  3+4     ergibt   7" << endl;
+    cout << "  10-5    ergibt   5" << endl;
+    cout << "  6*7     ergibt   42" << endl;
+    cout << "  15/3    ergibt   5" << endl;
+    cout << "  2.5*4   ergibt   10" << endl << endl;
+    cout << "Zum Beenden 'q' eingeben." << endl;
+    cout << "----------------------------------------" << endl << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-// Tips for Getting Started:
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main()
+{
+    printHelp();
+
+    string input;
+
+    while (true)
+    {
+        cout << "Eingabe: ";
+        cin >> input;
+
+        if (input == "q" || input == "Q" || input == "exit")
+        {
+            cout << "Auf Wiedersehen!" << endl;
+            break;
+        }
+
+        // Find operator (start at 1 to allow negative first number, e.g. -5+3)
+        size_t opPos = string::npos;
+        char op = 0;
+
+        for (size_t i = 1; i < input.size(); i++)
+        {
+            char c = input[i];
+            if (c == '+' || c == '-' || c == '*' || c == '/')
+            {
+                opPos = i;
+                op = c;
+                break;
+            }
+        }
+
+        if (opPos == string::npos || opPos == input.size() - 1)
+        {
+            cout << "Ungueltige Eingabe. Beispiel: 3+4" << endl << endl;
+            continue;
+        }
+
+        string leftStr  = input.substr(0, opPos);
+        string rightStr = input.substr(opPos + 1);
+
+        double a = 0.0, b = 0.0;
+        try
+        {
+            a = stod(leftStr);
+            b = stod(rightStr);
+        }
+        catch (...)
+        {
+            cout << "Ungueltige Zahlen. Beispiel: 3+4" << endl << endl;
+            continue;
+        }
+
+        if (op == '/' && b == 0.0)
+        {
+            cout << "Fehler: Division durch Null!" << endl << endl;
+            continue;
+        }
+
+        double result = 0.0;
+        switch (op)
+        {
+            case '+': result = a + b; break;
+            case '-': result = a - b; break;
+            case '*': result = a * b; break;
+            case '/': result = a / b; break;
+        }
+
+        cout << input << " = " << result << endl << endl;
+    }
+
+    return 0;
+}
